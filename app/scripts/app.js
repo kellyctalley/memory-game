16a52e4c584711fbe9b0086e03ea777c680e561a
Kelly Talley <kcutchen@gmail.com>
2015-08-25 02:14:43 +0000

    Cards are flipping and matching

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,187 @@
 //////////
+//shuffles cards
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+
+//////////
+//loads cards from json file and shuffles
+var cards;
+var cardData = [];
+$.getJSON('/js/cards.json', function(data) {
+  var firstCard, secondCard;
+  for (var i = 1, total = data.length; i < total; i++) {
+    firstCard = data[i];
+    firstCard.id = i;
+    cardData.push(firstCard);
+    secondCard = $.extend({}, firstCard);
+    secondCard.id = -i;
+    cardData.push(secondCard);
+  }
+
+  // randomize the array
+  shuffle(cardData);
+  cards = $(cardData);
+
+  var cardBackBase = $("<img />", {
+    src: "images/images/back.jpg",
+    class: "back"
+  });
+
+  var cardGame = $(".card-game");
+  // cardGame.empty()
+
+  cards.each(function (key, val) {
+    var cardFrontElement = $("<img />", {
+      src: val.image,
+      class: "front"
+    });
+
+    var cardBackElement = cardBackBase.clone();
+
+    var card = $("<div />", {
+      class: "card effect__click",
+      "data-id": val.id
+    });
+
+    card.append(cardBackElement);
+    card.append(cardFrontElement);
+
+    cardGame.append(card);
+  });
+
+
+
+
+///////
+//flip on click
+/*
+(function() {
+  var cards = document.querySelectorAll(".card.effect__click");
+  for ( var i  = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    clickListener( card );
+  }
+
+  function clickListener(card) {
+    card.addEventListener( "click", function() {
+      var c = this.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
+})();*/
+});
+
+
+
+
+//////////
+//creates click function for card
+$(function () {
+
+  var selectedCard = null;
+  var totalAttempts = 0;
+  var totalMatches = 0;
+
+  $(".card-game").on("click", ".card:not(.matched)", function (event) {
+    var card = $(this);
+    var front = card.find(".front");
+    var back = card.find(".back");
+    var cardId = card.data("id");
+    var src = front.attr("src");
+
+    if (!selectedCard) {
+      selectedCard = card;
+      console.log("src1");
+      card.addClass("flipped");
+    } else {
+      if (selectedCard.data("id") !== cardId) {
+        if (selectedCard.find(".front").attr("src") == src) {
+          console.log("match");
+          card.addClass("flipped");
+          card.addClass("matched");
+          selectedCard.addClass("matched");
+          selectedCard = null;
+          totalAttempts += 1
+          totalMatches += 1;
+          console.log(totalAttempts);
+          // check matched cards to number of cards total
+          // did they win?
+        } else {
+          card.addClass("flipped");
+          console.log("no match");
+          totalAttempts += 1;
+          console.log(totalAttempts);
+          setTimeout(function () {
+            card.removeClass("flipped");
+            selectedCard.removeClass("flipped");
+            selectedCard = null;
+          }, 500);
+        }
+      } else {
+        card.removeClass("flipped");
+        selectedCard = null;
+      }
+    }
+
+/*
+    if (!isFirstClick) {
+      var src1 = front.attr("src");
+      console.log("src1");
+      console.log(src1);
+      isFirstClick = true;
+    } else {
+      var src2 = front.attr("src");
+      console.log("src2");
+      console.log (src2);
+    }*/
+
+    
+
+
+  });
+});
+
+
+//card is clicked
+  //if no info is stored, it is first click
+
+//if first click
+  //src info is stored in variable
+//if second click
+  //src info is stored in variable and compared to first variable
+
+//if cards do not match
+  //cards turn over after 2-3 seconds
+  //attempt is logged to attempts count
+//if cards match
+  //cards stay turned over
+  //attempt is logged to attempts count
+  //match is logged to matches count
+
+
+
+
+
+//just in case//
+//////////
 //creates shuffle function
 /*
 (function($){
@@ -39,122 +222,3 @@ function findAndReplace(find, replace) {
  
 })(jQuery);
 */
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-
-
-//////////
-//loads cards from json file and shuffles
-var cards;
-var cardData = [];
-$.getJSON('/js/cards.json', function(data) {
-  var card;
-  for (var i = 0, total = data.length; i < total; i++) {
-    card = data[i];
-
-    cardData.push(card, card);
-  }
-
-  // randomize the array
-  shuffle(cardData);
-  cards = $(cardData);
-
-  var cardBackBase = $("<img />", {
-    src: "images/images/back.jpg",
-    class: "back"
-  });
-
-  var cardGame = $(".card-game");
-  // cardGame.empty()
-
-  cards.each(function (key, val) {
-    var cardFrontElement = $("<img />", {
-      src: val.image,
-      class: "front"
-    });
-
-    var cardBackElement = cardBackBase.clone();
-
-    var card = $("<div />", {
-      class: "card effect__click"
-    });
-
-    card.append(cardBackElement);
-    card.append(cardFrontElement);
-
-    cardGame.append(card);
-  });
-
-
-
-
-///////
-//flip on click
-(function() {
-  var cards = document.querySelectorAll(".card.effect__click");
-  for ( var i  = 0, len = cards.length; i < len; i++ ) {
-    var card = cards[i];
-    clickListener( card );
-  }
-
-  function clickListener(card) {
-    card.addEventListener( "click", function() {
-      var c = this.classList;
-      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
-    });
-  }
-})();
-});
-
-
-
-
-//////////
-//creates click function for card
-$(function () {
-  $(".card-game").on("click", ".card", function (event) {
-    var card = $(event.target);
-    var front = $(this).find(".front");
-    var back = card.find(".back");
-    var src = front.attr("src");
-    console.log(src);
-    //console.log(card.attr("src"));
-  });
-});
-
-
-
-
-
-
-
-//when they click
-//  if info is not saved, it is the first click, so we save the info
-//  if info is saved, it is the second click, so we compare
-//  if it is a match ...
-//  if it is not ... 
-//  if they have them all, they win
-
-// how to handle if they try to click the same element more than once
-
-//first click store src info
-//second will check if info stored and check for match
-//if info
