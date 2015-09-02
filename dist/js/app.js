85f75d4ee69a2287b0d4b6e600eeafe7e1d16cd4
Kelly Talley <kcutchen@gmail.com>
2015-09-02 02:41:57 +0000

    Game complete

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,13 +1,3 @@
-$(function () {
- $(".you-win").hide();
- $(".card-game").hide();
-
- $(".start-game").on("click", function() {
-  $(".welcome").hide();
-  $(".card-game").show();
- });
-});
-
 //////////
 //shuffles cards
 function shuffle(array) {
@@ -30,11 +20,15 @@ function shuffle(array) {
 }
 
 
-
 //////////
 //loads cards from json file and shuffles
+function playGame() {
 var cards;
 var cardData = [];
+
+  $(".welcome").hide();
+  $(".card-game").show();
+
 $.getJSON('/js/cards.json', function(data) {
   var firstCard, secondCard;
   for (var i = 1, total = data.length; i < total; i++) {
@@ -76,31 +70,7 @@ $.getJSON('/js/cards.json', function(data) {
 
     cardGame.append(card);
   });
-
-
-
-
-///////
-//flip on click
-/*
-(function() {
-  var cards = document.querySelectorAll(".card.effect__click");
-  for ( var i  = 0, len = cards.length; i < len; i++ ) {
-    var card = cards[i];
-    clickListener( card );
-  }
-
-  function clickListener(card) {
-    card.addEventListener( "click", function() {
-      var c = this.classList;
-      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
-    });
-  }
-})();*/
 });
-
-
-
 
 //////////
 //creates click function for card
@@ -139,9 +109,9 @@ $(function () {
           $("#attempts").html(totalAttempts);
           // check matched cards to number of cards total
           // did they win?
-          if (totalMatches === 15) {
-            $(".card-game").hide(500);
-            $(".you-win").show(2000);
+          if (totalMatches === cardData.length/2) {
+            $(".card-game").hide();
+            $(".you-win").show();
           }
         } else {
           card.addClass("flipped");
@@ -152,7 +122,7 @@ $(function () {
             card.removeClass("flipped");
             selectedCard.removeClass("flipped");
             selectedCard = null;
-          }, 500);
+          }, 600);
         }
       } else {
         card.removeClass("flipped");
@@ -160,84 +130,24 @@ $(function () {
       }
     }
 
-/*
-    if (!isFirstClick) {
-      var src1 = front.attr("src");
-      console.log("src1");
-      console.log(src1);
-      isFirstClick = true;
-    } else {
-      var src2 = front.attr("src");
-      console.log("src2");
-      console.log (src2);
-    }*/
-
-    
-
-
   });
 });
-
-
-//card is clicked
-  //if no info is stored, it is first click
-
-//if first click
-  //src info is stored in variable
-//if second click
-  //src info is stored in variable and compared to first variable
-
-//if cards do not match
-  //cards turn over after 2-3 seconds
-  //attempt is logged to attempts count
-//if cards match
-  //cards stay turned over
-  //attempt is logged to attempts count
-  //match is logged to matches count
-
-
-
-
-
-//just in case//
-//////////
-//creates shuffle function
-/*
-(function($){
- 
-    $.fn.shuffle = function() {
-
-  var elements = this.get()
-  var copy = [].concat(elements)
-  var shuffled = []
-  var placeholders = []
-
-  // Shuffle the element array
-  while (copy.length) {
-    var rand = Math.floor(Math.random() * copy.length)
-    var element = copy.splice(rand,1)[0]
-    shuffled.push(element)
-  }
-
-  // replace all elements with a plcaceholder
-  for (var i = 0; i < elements.length; i++) {
-    var placeholder = document.createTextNode('')
-    findAndReplace(elements[i], placeholder)
-    placeholders.push(placeholder)
-  }
-
-  // replace the placeholders with the shuffled elements
-  for (var i = 0; i < elements.length; i++) {
-    findAndReplace(placeholders[i], shuffled[i])
-  }
-
-  return $(shuffled)
-
 }
 
-function findAndReplace(find, replace) {
-  find.parentNode.replaceChild(replace, find)
-}
- 
-})(jQuery);
-*/
+
+
+$(function () {
+ $(".you-win").hide();
+ $(".card-game").hide();
+
+ $(".start-game").on("click", function() {
+  playGame();
+ });
+
+ $(".play-again").on("click", function() {
+    $(".card-game").empty();
+    $(".stats-box").empty();
+    playGame();
+  });
+
+});
